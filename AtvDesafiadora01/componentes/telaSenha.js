bc045734848fd48bb275d3c988d9be7cf0cf1eec
cortezinho <gabrielcortez2802@gmail.com>
2025-04-07 19:28:29 -0300

    tbdbz

---
+++ AtvDesafiadora01/componentes/telaSenha.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {View, Text, Button, StatusBar} from "react-native"
+import styles from "./styles"
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+
+type Props = NativeStackScreenProps<RootStackParamList>
+
+export default function Home({navigation} : Props){
+    return(
+        <View styles = {styles.container}>
+            <StatusBar barStyle = "dark-content"/>
+            <Text>Home Screen</Text>
+            <Button title = "Settings" onPress={() => navigation.navigate("Settings")}/>
+        </View>
+    )
+}
